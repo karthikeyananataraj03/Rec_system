73ad381d052097b7136b354c9b4124e9bf2f082b
karthikeyananataraj03 <120604313+karthikeyananataraj03@users.noreply.github.com>
2022-12-23 02:14:37 -0500

    Add files via upload

--- Rec_Python.py
+++ Rec_Python.py
@@ -57,9 +57,9 @@ def content_based(bookTitle):
         if bookTitle in rare_books:
             most_common=pd.Series(common_books["Book-Title"].unique()).sample(3).values
             st.success(f'Could Find any good recommendation but here are some top picks') 
-            st.success(f'Pick 1 ${most_common[0]:.2f} people')
-            st.success(f'Pick 2 ${most_common[1]:.2f} people')
-            st.success(f'Pick 3 ${most_common[2]:.2f} people') 
+            st.success('Pick 1 '+ most_common[0])
+            st.success('Pick 2' + most_common[1])
+            st.success('Pick 3 '+ most_common[2])  
         else:
             common_books=common_books.drop_duplicates(subset=["Book-Title"])
             common_books.reset_index(inplace=True)
@@ -77,12 +77,12 @@ def content_based(bookTitle):
                 
                 books.append(common_books[common_books["index"]==similar_booksSorted[i][0]]["Book-Title"].item())
       
-            st.success(f'Pick 1 ${books[0]:.2f} people')
-            st.success(f'Pick 2 ${books[1]:.2f} people')
-            st.success(f'Pick 3 ${books[2]:.2f} people')  
+            st.success('Pick 1 '+ books[0])
+            st.success('Pick 2' + books[1])
+            st.success('Pick 3 '+ books[2])  
           
     else:
-            st.success(f'Could Find any good recommendation')                       
+            st.success('Could Find any good recommendation')                       
 
 
 
